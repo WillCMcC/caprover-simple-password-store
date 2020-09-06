ce39a3b03b8cfff64bd6da4395f8ccbee7882c9c
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2020-09-06 16:30:55 -0700

    ok

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+let secret = process.env.SECRET
+
+let passMap = process.env
+
+
+app.post('/', express.json(), function(req, res) {
+
+  let keys = Object.keys(req.body);
+
+  if(passMap[keys[0]] === req.body[keys[0]]){
+    return res.json(true)
+  }else{
+    return res.json(false)
+  }
 });
+
 app.listen(9000);
