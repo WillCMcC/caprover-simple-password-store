0cbe6cfebb63761b793618d13ea5086268690911
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2020-09-06 17:01:18 -0700

    ok

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-let secret = process.env.SECRET
 let passMap = process.env
+let valueMap = {}
+let keys = Object.keys(passMap)
+
+for(let keyIndex in keys){
+
+  let key = keys[keyIndex]
+
+  if(keys.indexOf(`return_${key}`) > -1){
+    valueMap[key] = passMap[`return_${key}`]
+  }
+}
 
 app.post('/', express.json(), function(req, res) {
 
   let keys = Object.keys(req.body);
 
   if(passMap[keys[0]] === req.body[keys[0]]){
+
+    if(valueMap[keys[0]]){
+       return res.json(valueMap[keys[0]])
+    }
     return res.json(true)
   }else{
     return res.json(false)
